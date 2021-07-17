302992ec09b90ffdedf8ca77cb6f95ebbcd3b572
BarrierAntho <72570614+BarrierAntho@users.noreply.github.com>
2021-07-17 20:31:48 +0200

    Update C_07_main.c

--- C_07_main.c
+++ C_07_main.c
@@ -3,19 +3,19 @@
 #include <string.h>
 #include <limits.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 // Ex00
 char *ft_strdup(char *src);
 // Ex01
 int *ft_range(int min, int max);
 // Ex02
-
+int ft_ultimate_range(int **range, int min, int max);
 // Ex03
-
+char *ft_strjoin(int size, char **strs, char *sep);
 // Ex04
 
 // Ex05
-
 
 int	main(void)
 {
@@ -24,31 +24,31 @@ int	main(void)
 	printf("###############################\n");
 	printf("Ex00\n");
 
-	#define EX00_S	4
-	char	txt001[EX00_S][CHAR_MAX] = {"42", "Hello", "World", ""};
+	#define EX00_S	5
+	char	txt001[EX00_S][CHAR_MAX] = {"42", "Hello", "World", "", "toto"};
 	char	*ptr001;
 	char	*ptr002;
-	int		i01 = 0;
+	int		i00 = 0;
 	ptr001 = 0;
 	ptr002 = 0;
 
-	while (i01 < EX00_S)
+	while (i00 < EX00_S)
 	{
-		printf("ORIGINAL : i_01 = %d\n", i01);
-		printf("BEFORE : txt = %s\tptr = %s\n", txt001[i01], ptr001);
-		ptr001 = strdup(txt001[i01]);
-		printf("AFTER : txt = %s\tptr = %s\n", txt001[i01], ptr001);
+		printf("ORIGINAL : i_01 = %d\n", i00);
+		printf("BEFORE : txt = %s\tptr = %s\n", txt001[i00], ptr001);
+		ptr001 = strdup(txt001[i00]);
+		printf("AFTER : txt = %s\tptr = %s\n", txt001[i00], ptr001);
 
 		printf("------------------------------\n");
 
-		printf("MYFUNC : i_01 = %d\n", i01);
-		printf("BEFORE : txt = %s\tptr = %s\n", txt001[i01], ptr002);
-		ptr002 = ft_strdup(txt001[i01]);
-		printf("AFTER : txt = %s\tptr = %s\n", txt001[i01], ptr002);
+		printf("MYFUNC : i_01 = %d\n", i00);
+		printf("BEFORE : txt = %s\tptr = %s\n", txt001[i00], ptr002);
+		ptr002 = ft_strdup(txt001[i00]);
+		printf("AFTER : txt = %s\tptr = %s\n", txt001[i00], ptr002);
 
 		strcmp(ptr001, ptr002) == 0  ? printf("SUCCES\n") : printf("FAILED\n");
 
-		i01++;
+		i00++;
 
 		printf("******************************\n******************************\n\n");
 	}
@@ -59,29 +59,32 @@ int	main(void)
 	printf("###############################\n");
 	printf("Ex01\n");
 
-	#define EX01_S	3
-	int	min[EX01_S] = {0, 0, -10, INT_MIN};
-	int	max[EX01_S] = {10, 1, 10, INT_MAX};
+	#define EX01_S	5
+	int	min01[EX01_S] = {0, 0, -10, 0, -20};
+	int	max01[EX01_S] = {10, 1, 10, -5, -10};
 	int	*ptr011;
-	int	i02 = 0;
-	int j02 = 0;
+	int	i01 = 0;
+	int j01 = 0;
 	ptr011 = 0;
+	int	k01;
 
-	while (i02 < EX01_S)
+	while (i01 < EX01_S)
 	{
+		printf("BEFORE ptr = %p\n", ptr011);
+		printf("min = %d\tmax = %d\n", min01[i01], max01[i01]);
+		ptr011 = ft_range(min01[i01], max01[i01]);
 
-		printf("min = %d\tmax = %d\n", min[i02], max[i02]);
-
-		ptr011 = ft_range(min[i02], max[i02]);
-		j02 = 0;
-		while (ptr011[j02])
+		j01 = min01[i01];
+		k01 = 0;
+		while (j01 < max01[i01])
 		{
-			printf("nb = %d\n", ptr011[j02]);
-			j02++;
+			printf("nb = %d\n", ptr011[k01]);
+			j01++;
+			k01++;
 		}
 
-		i02++;
-		ptr011 = 0;
+		i01++;
+		printf("AFTER ptr = %p\n", ptr011);
 
 		printf("******************************\n******************************\n\n");
 	}
@@ -91,14 +94,49 @@ int	main(void)
 //////////////////////////////////////////////////
 	printf("###############################\n");
 	printf("Ex02\n");
+	#define EX02_S	5
+	int	min02[EX02_S] = {0, 0, -10, 0, -20};
+	int	max02[EX02_S] = {10, 1, 10, -5, -10};
+	int	*ptr021;
+	int	res02 = 0;
+	int	i02 = 0;
+	int j02 = 0;
+	ptr021 = 0;
+	int	k02;
 
+	while (i02 < EX02_S)
+	{
+		printf("BEFORE ptr = %p\n", ptr021);
+		printf("min = %d\tmax = %d\n", min02[i02], max02[i02]);
+		res02 = ft_ultimate_range(&ptr021, min02[i02], max02[i02]);
+
+		j02 = min02[i02];
+		k02 = 0;
+		while (j02 < max02[i02])
+		{
+			printf("nb = %d\n", ptr021[k02]);
+			j02++;
+			k02++;
+		}
+		i02++;
+		printf("AFTER ptr = %p\n", ptr021);
+		printf("[main] res02 = %d\n", res02);
+
+		printf("******************************\n******************************\n\n");
+	}
 
 	printf("\n");
 
 //////////////////////////////////////////////////
 	printf("###############################\n");
 	printf("Ex03\n");
+	#define EX03_S	5
+	char	str03[EX03_S][INT_MAX] = {"Hello", "World", "of", "Warcraft", "!"};
+	char	*res03;
+	const char	sep03	= "_";
 
+	res03 = strjoin(sep03, str03[0], str03[1], str03[2], str03[3], str03[4]);
+	printf("res03 = %s\n", res03);
 
 	printf("\n");
 
